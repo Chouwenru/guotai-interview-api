a6cca593a0ba523700c946e59f3e9637d74671e0
eric <ericode200@gmail.com>
2025-10-25 21:03:52 +0800

    - add : CoinDesk Feature  - delete : test api in Spring Application

---
+++ src/main/java/com/example/demo/entity/CoinDesk.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+//@Entity
+//@Table(name = "coinDesk")
+public class CoinDesk {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
+
+
