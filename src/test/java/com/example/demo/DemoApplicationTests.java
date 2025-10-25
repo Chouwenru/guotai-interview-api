45fa62150afbba480ad13bc2c5ca6281a68ac278
asus\user <chou86108@gmail.com>
2025-10-25 20:31:06 +0800

    test commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
@@ -21,5 +22,6 @@ class DemoApplicationTests {
 	void homeResponse() {
 		String body = this.restTemplate.getForObject("/", String.class);
 		assertEquals("Spring is here!", body);
+		
 	}
 }
